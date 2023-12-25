1c5d935a7126f8d668734566d7653d34478c2bad
vedant312 <vedantd20@iitk.ac.in>
2023-12-25 19:40:00 +0530

    Add loading spinner to user retrieval and user list components

--- src/components/user/RetrieveUser.jsx
+++ src/components/user/RetrieveUser.jsx
@@ -3,16 +3,20 @@ import { useParams } from 'react-router-dom';
 import UserCard from './Card';
 import Layout from '../layout/Layout';
 import * as userService from '../../services/user.service';
+import { Spinner } from 'react-bootstrap';
 
 const RetriveUser = () => {
   const { id } = useParams();
   const [user, setUser] = useState({});
+  const [isloading, setIsLoading] = useState(false);
 
   const fetchUser = async () => {
     try {
+      setIsLoading(true);
       const res = await userService.retriveUser(id);
 
       setUser(res.data.data);
+      setIsLoading(false);
     } catch (err) {
       setUser(null);
     }
@@ -27,14 +31,20 @@ const RetriveUser = () => {
       <Layout>
         <h3 className='text-center mt-3 mb-3'>Requested User</h3>
         {user ? (
-          <UserCard
-            key={user.id}
-            id={user.id}
-            name={user.name}
-            email={user.email}
-            city={user.city}
-            country={user.country}
-          />
+          isloading ? (
+            <div class='d-flex justify-content-center'>
+              <Spinner />
+            </div>
+          ) : (
+            <UserCard
+              key={user.id}
+              id={user.id}
+              name={user.name}
+              email={user.email}
+              city={user.city}
+              country={user.country}
+            />
+          )
         ) : (
           <h5 className='text-center text-danger'>User not found</h5>
         )}
--- src/components/user/UsersList.jsx
+++ src/components/user/UsersList.jsx
@@ -2,15 +2,19 @@ import React, { useEffect, useState } from 'react';
 import UserCard from './Card';
 import Layout from '../layout/Layout';
 import * as userService from '../../services/user.service';
+import { Spinner } from 'react-bootstrap';
 
 const UsersList = () => {
   const [users, setUsers] = useState({});
+  const [isloading, setIsLoading] = useState(false);
 
   const fetchUsers = async () => {
     try {
+      setIsLoading(true);
       const res = await userService.retrieveAllUsers();
       console.log(res);
       setUsers(res.data.data);
+      setIsLoading(false);
     } catch (err) {
       setUsers(null);
     }
@@ -24,18 +28,24 @@ const UsersList = () => {
     <Layout>
       <h3 className='text-center mt-3 mb-3'>Users</h3>
       {users ? (
-        Object.values(users).map((user) => {
-          return (
-            <UserCard
-              key={user.id}
-              id={user.id}
-              name={user.name}
-              email={user.email}
-              city={user.city}
-              country={user.country}
-            />
-          );
-        })
+        isloading ? (
+          <div class='d-flex justify-content-center'>
+            <Spinner />
+          </div>
+        ) : (
+          Object.values(users).map((user) => {
+            return (
+              <UserCard
+                key={user.id}
+                id={user.id}
+                name={user.name}
+                email={user.email}
+                city={user.city}
+                country={user.country}
+              />
+            );
+          })
+        )
       ) : (
         <h5 className='text-center text-danger'>No users found</h5>
       )}
